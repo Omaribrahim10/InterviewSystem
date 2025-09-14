ad6e037b24eb6554a1346134d8f6ea2f7b78d6aa
Omaribrahim10 <omarr.ibrahiimm@gmail.com>
2025-09-14 14:22:16 +0300

    Update Program.cs and appsettings.json for MySQL Railway

--- InterviewsApplication/Migrations/20250914111144_InitialCreate_MySql.Designer.cs
+++ InterviewsApplication/Migrations/20250914111144_InitialCreate_MySql.Designer.cs
@@ -4,6 +4,7 @@ using InterviewsApplication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InterviewsApplication.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250914111144_InitialCreate_MySql")]
+    partial class InitialCreate_MySql
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- InterviewsApplication/wwwroot/assets/static/js/Logout.js
+++ InterviewsApplication/wwwroot/assets/static/js/Logout.js
@@ -19,7 +19,7 @@
                 sessionStorage.clear();
                 localStorage.clear();
 
-                window.location.href = "https://localhost:7286/dashboard.html";
+                window.location.href = "https://localhost:7286/index.html";
             } catch (error) {
                 console.error("Logout error:", error);
                 alert("Something went wrong during logout.");
